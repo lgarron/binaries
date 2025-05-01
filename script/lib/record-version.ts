989673482cec18a6d9cff4129b8b6d12ccaa2430
Lucas Garron <code@garron.net>
2025-05-01 22:20:47 +0000

    Format the JSON file.

--- script/lib/record-version.ts
+++ script/lib/record-version.ts
@@ -1,8 +1,9 @@
-import { file } from "bun";
+import { $, file } from "bun";
 
 export async function recordVersion(key: string, value: string) {
   const versionsJSONFile = file("./versions.json");
   const versionsJSON = await versionsJSONFile.json();
   versionsJSON[key] = value;
   await versionsJSONFile.write(JSON.stringify(versionsJSON, null, "  "));
+  await $`bun x @biomejs/biome check --write ./versions.json`;
 }

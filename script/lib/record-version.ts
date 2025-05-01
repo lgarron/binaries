31d33947232a46c14e2747a47ecccce71607dc25
Lucas Garron <code@garron.net>
2025-05-01 22:04:25 +0000

    [record-version.ts] grrr, floating `Promise`.

--- script/lib/record-version.ts
+++ script/lib/record-version.ts
@@ -4,5 +4,5 @@ export async function recordVersion(key: string, value: string) {
   const versionsJSONFile = file("./versions.json");
   const versionsJSON = await versionsJSONFile.json();
   versionsJSON[key] = value;
-  versionsJSONFile.write(JSON.stringify(versionsJSON, null, "  "));
+  await versionsJSONFile.write(JSON.stringify(versionsJSON, null, "  "));
 }

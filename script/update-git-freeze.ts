7f72e39c52c5fc4a371bad082d8310dd96d69f6f
Lucas Garron <code@garron.net>
2025-05-01 18:58:12 +0000

    Add `git-freeze` version.

--- script/update-git-freeze.ts
+++ script/update-git-freeze.ts
@@ -1,8 +1,13 @@
 #!/usr/bin/env bun
 
 import { $ } from "bun";
+import { recordVersion } from "./lib/record-version";
+
+const { sha } = await (await fetch("https://api.github.com/repos/lgarron/git-freeze/commits/HEAD")).json();
 
 for (const fileName of ["git-abandon", "git-freeze", "git-thaw"]) {
-  await $`curl "https://raw.githubusercontent.com/lgarron/git-freeze/main/bin/${fileName}" --output ./linux-x64/${fileName}`;
+  await $`curl "https://raw.githubusercontent.com/lgarron/git-freeze/${sha}/bin/${fileName}" --output ./linux-x64/${fileName}`;
   await $`chmod +x ./linux-x64/${fileName}`;
 }
+
+recordVersion("git-freeze", sha);

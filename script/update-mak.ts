9cb0954d6dfefe5c31e8690742c43f01c82f93fa
Lucas Garron <code@garron.net>
2025-12-25 03:38:52 -0800

    [update-mak.ts] Install `mak` from `git`.

--- script/update-mak.ts
+++ script/update-mak.ts
@@ -3,7 +3,7 @@
 import { $ } from "bun";
 import { recordVersion } from "./lib/record-version";
 
-await $`cargo install --force --target-dir ./.temp/mak/ mak`;
+await $`cargo install --force --target-dir ./.temp/mak/ --git https://github.com/lgarron/mak`;
 await $`cp ./.temp/mak/release/mak ./linux-x64/mak`;
 
 // TODO: why does this output `fake` rather than `mak` on Linux?
